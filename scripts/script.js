7f55943467fd6149ae5e5cf24d581e0a73864894
ForSureN1 <standin1532@gmail.com>
2021-09-01 10:45:46 +0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => { 
+	
+	// Burger menu
 	const burger = document.querySelector('.burger');
 	const menu = document.querySelector('.menu__items')
 	burger.addEventListener('click', ()=>{
@@ -6,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		menu.classList.toggle('active');
 	});
 
-
+	// accordion
 	const accordTitles = document.querySelectorAll('.elem-top');
 
 	accordTitles.forEach(title => {
@@ -17,4 +19,31 @@ document.addEventListener("DOMContentLoaded", () => {
 	    currentTarget.nextElementSibling.classList.toggle('active_text');
 	    currentTarget.querySelector('.faq__arrow').classList.toggle('arrow_rotate');
 	};
+
+	//busket
+	// let cartData = new getCartData() || {};
+
+	let cartData = {};
+	let buy = document.querySelectorAll('.buy')
+	for (let i = 0; i < buy.length; i++) {
+		buy[i].addEventListener('click', ()=>{
+			let itemId = buy[i].getAttribute('data-id');
+			let ItemPrice = buy[i].getAttribute('data-price')
+			cartData = {
+				'id': buy[i].dataset.id,
+				'price': buy[i].dataset.price,
+			}
+			console.log(cartData);
+			setCartData(cartData);
+		})
+	}
+	function getCartData() {
+		return JSON.parse(localStorage.getItem('Swarovski'));
+	}
+	function setCartData(o){
+		localStorage.setItem('Swarovski', JSON.stringify(o));
+		return false;
+	}
+
+
  });
