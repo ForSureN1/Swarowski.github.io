3f695933830ad2b8fe484fe7acd9c44f4276dbda
ForSureN1 <standin1532@gmail.com>
2021-09-03 18:07:58 +0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -20,155 +20,135 @@ document.addEventListener("DOMContentLoaded", () => {
 	    currentTarget.querySelector('.faq__arrow').classList.toggle('arrow_rotate');
 	};
 
-	//language
-
-	const lang = document.querySelector('.lang_choice');
-	lang.addEventListener('click', ()=>{
-		this.siblings('other_lang').slideToggle();
-		this.parent('lang_block').toggleClass('active');
-	})
-
-
-
+	
 	//busket
 
-
-	// //Переменные
-	// let cartData = {};
-	// let buy = document.querySelectorAll('.buy')
-
-	// //Вешаем обр. собатый на каждую кпопку с вызовом функции
-	// for (let i = 0; i < buy.length; i++) {
-	// 	buy[i].addEventListener('click', getAttr)
-	// }
-
-
-	// //Получаем атрибуты товара и записываем их в объект
-	// function getAttr(){
-	// 	let itemId = this.getAttribute('data-id');
-	// 	let itemPrice = this.getAttribute('data-price');
-	// 	cartData = {
-	// 			'id': this.dataset.id,
-	// 			'price': this.dataset.price,
-	// 		}
-	// 	setCartData(cartData);
-	// }
-
-	// //Получение данных из LocalStorage
-	// function getCartData() {
-	// 	return JSON.parse(localStorage.getItem('Swarovski'));
-	// }
-
-
-	// //Запись данных в LocalStorage
-	// function setCartData(o){
-	// 	localStorage.setItem('Swarovski', JSON.stringify(o));
-	// 	return false;
-	// }
-
- //    function getData(e) {
- //    	let cartData = getCartData();
- //    	console.log(cartData);       	
-	// }
-
-
+	//Вызов функции товара
 	function requestCart() {
-		const cartDOMElement = document.querySelector('.js-cart')
-		const cart = {};
-
-		if(!cartDOMElement) {
-			return;
-		}
-
-		const renderCartItem = ({articul, name, desc, color, size, price,totalprice, src, quantity}) => {
-			const cartItemDOMElement = document.createElement('tr');
-
-			const cartItemTemplate = `
-				<tr class="product__item">
-					<td class="product__title">
-						<a href="#">${src}</a>
-					</td>
-					<td class="product__name">
-						<h2>${name}</h2>
-						<p>${desc}</p>
-					</td>
-					<td class="product__size-color">
-						<p>${size}</p>
-					</td>
-					<td class="product__count">
-						<p>${quantity}</p>
-					</td>
-					<td class="product__price">
-						<p>${price}</p>
-					</td>
-					<td class="product__summ">
-						<p>${totalprice}</p>
-					</td>
-					<td class="product__remove">
-						<a href="#" class="remove"><img src="images/remove.svg" alt=""></a>
-					</td>
-				</tr>
-			`;
-			cartItemDOMElement.innerHTML = cartItemTemplate;
-			cartItemDOMElement.setAttribute('data-product-articul', articul)
-			cartItemDOMElement.classList.add('js-cart-item')
-			cartDOMElement.appendChild(cartItemDOMElement);
-		}
-
-		const addCartItem = (data) => {
-			const {articul} = data;
-			cart[articul] = data;
-
-			renderCartItem(data);
-		}
+    const cartDOMElement = document.querySelector('.js-cart')
+    const cart = JSON.parse(localStorage.getItem('cart')) || {};
 
 
-		const getProductData = (productDOMElement) => {
-			const name = productDOMElement.getAttribute('data-product-name');
-			const desc = productDOMElement.getAttribute('data-product-desc');
-			const articul = productDOMElement.getAttribute('data-product-articul');
-			const size = productDOMElement.getAttribute('data-product-size');
-			const color = productDOMElement.getAttribute('data-product-color');
-			const price = productDOMElement.getAttribute('data-product-price');
-			const src = productDOMElement.getAttribute('data-product-img');
-			const quantity = 1;
-			const totalprice = quantity * +(price);
-			return  {
-				name,
-				desc,
-				articul,
-				size,
-				color,
-				price,
-				totalprice,
-				src,
-				quantity,
-			};
-		}
+    //отображаем добавленный товар в корзине
+    const renderCartItem = ({articul, name, desc, color, size, price,totalprice, src, quantity}) => {
+      const cartItemDOMElement = document.createElement('tr');
+
+      const cartItemTemplate = `
+        <tr class="product__item">
+          <td class="product__title">
+            <a href="#"><img src="${src}"></a>
+          </td>
+          <td class="product__name">
+            <h2>${name}</h2>
+            <p>${desc}</p>
+          </td>
+          <td class="product__size-color">
+            <p>${size}</p>
+            <p>${color}</p>
+          </td>
+          <td class="product__count">
+            <p>${quantity}</p>
+          </td>
+          <td class="product__price">
+            <p>${price}</p>
+          </td>
+          <td class="product__summ">
+            <p>${totalprice}</p>
+          </td>
+          <td class="product__remove">
+            <button class="remove buy"><img src="images/remove.svg" alt=""></button>
+          </td>
+        </tr>
+      `;
+      cartItemDOMElement.innerHTML = cartItemTemplate;
+      cartItemDOMElement.setAttribute('data-product-articul', articul);
+      cartItemDOMElement.classList.add('js-cart-item');
+      cartDOMElement.appendChild(cartItemDOMElement);
+    }
 
 
-		const cartInit = () => {
-			document.querySelector('body').addEventListener('click', (e) => {
-				const target = e.target;
-				if (target.classList.contains('js-buy')) {
-					e.preventDefault();
-					const productDOMElement = target.closest('.js-product');
-					const data = getProductData(productDOMElement);
-					addCartItem(data);
-				}
-			});
-		}
+    //сохраняем товар в LocalStorage
+    const saveCart = () => {
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }
 
-		cartInit();
+    //Обновляем данные в LocalStorage
+    const updateCart = () => {
+      console.log(cart);
+      saveCart();
+    }
+
+
+    //Удаление из корзины
+    const deleteCartItem = (articul) => {
+      const cartItemDOMElement = cartDOMElement.querySelector(`[data-product-articul="${articul}"]`);
+      cartDOMElement.removeChild(cartItemDOMElement);
+      delete cart[articul];
+      updateCart();
+      console.log(cartItemDOMElement)
+    }
+
+    //Добавление в корзину
+    const addCartItem = (data) => {
+      // const {articul} = data;
+      // cart[articul] = data;
+      // updateCart();
+      // renderCartItem(data);
+      const {articul} = data;
+      cart[articul] = data;
+      updateCart();
+      if (cartDOMElement) {
+        renderCartItem(data);
+      }
+    }
+
+    //Получаем данные для объекта
+    const getProductData = (productDOMElement) => {
+      const name = productDOMElement.getAttribute('data-product-name');
+      const desc = productDOMElement.getAttribute('data-product-desc');
+      const articul = productDOMElement.getAttribute('data-product-articul');
+      const size = productDOMElement.getAttribute('data-product-size');
+      const color = productDOMElement.getAttribute('data-product-color');
+      const price = productDOMElement.getAttribute('data-product-price');
+      const src = productDOMElement.getAttribute('data-product-img');
+      const quantity = 1;
+      const totalprice = quantity * +(price);
+      return  { name, desc, articul, size, color, price, totalprice, src, quantity,};
+    }
+
+    const renderCart = () => {
+    	const ids = Object.keys(cart);
+    	// console.log(ids);
+    	ids.forEach((articul) => renderCartItem(cart[articul]));
+    };
+
+
+    //Инициализация
+    const cartInit = () => {
+    	if (cartDOMElement) {
+        	renderCart();
+    	}
+    	document.querySelector('body').addEventListener('click', (e) => {
+    		const target = e.target;
+    		if (target.classList.contains('js-buy')) {
+    			e.preventDefault();
+    			const productDOMElement = target.closest('.js-product');
+    			const data = getProductData(productDOMElement);
+    			addCartItem(data);
+    		}
+    		if (target.classList.contains('remove')) {
+    			e.preventDefault();
+    			const cartItemDOMElement = target.closest('.js-cart-item');
+    			const productArticul = cartItemDOMElement.getAttribute('data-product-articul');
+    			deleteCartItem(productArticul);
+    		}
+    	});
 	}
 
-	requestCart();
-	
+    cartInit();
+  }
 
-
-
-
-
+  requestCart();
 
 
 
